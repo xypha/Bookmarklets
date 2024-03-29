1fdd19d1908257378c9d4ae9f145284d37594ca2
xypha <12472214+xypha@users.noreply.github.com>
2024-03-29 16:46:16 +0530

    Update Set_Invidious_preferences_in_two_clicks.js

--- Set_Invidious_preferences_in_two_clicks.js
+++ Set_Invidious_preferences_in_two_clicks.js
@@ -1,7 +1,10 @@
 /* 
 https://github.com/xypha/Bookmarklets/edit/main/Set_Invidious_preferences_in_two_clicks.js
 Last Updated: 2024.03.29
- */
+
+Minified @ https://www.minifier.org/
+javascript:(function(){document.getElementById("video_loop").checked=!1;document.getElementById("autoplay").checked=!1;document.getElementById("continue").checked=!1;document.getElementById("continue_autoplay").checked=!1;document.getElementById("local").checked=!1;document.getElementById("listen").checked=!1;document.getElementById("speed").options[4].selected=!0;document.getElementById("quality").options[1].selected=!0;document.getElementById("quality_dash").options[6].selected=!0;document.getElementById("volume").value="50";document.getElementById("comments[0]").options[0].selected=!0;document.getElementById("comments[1]").options[0].selected=!0;document.getElementById("captions[0]").options[4].selected=!0;document.getElementById("captions[1]").options[2].selected=!0;document.getElementById("captions[2]").options[1].selected=!0;document.getElementById("related_videos").checked=!0;document.getElementById("annotations").checked=!1;document.getElementById("extend_desc").checked=!0;document.getElementById("vr_mode").checked=!1;document.getElementById("save_player_pos").checked=!0;document.getElementById("automatic_instance_redirect").checked=!0})()
+*/
 
 javascript: (function () {
     document.getElementById("video_loop").checked = false;                 // Always loop
