3046b05f519098b815cd2f960f19c397bd0ebff3
xypha <12472214+xypha@users.noreply.github.com>
2024-03-29 16:54:58 +0530

    Update Open_IMDb_trailer_in_new_tab.js
    add comments and minified version

--- Open_IMDb_trailer_in_new_tab.js
+++ Open_IMDb_trailer_in_new_tab.js
@@ -1,5 +1,13 @@
+/*
+https://github.com/xypha/Bookmarklets/edit/main/Open_IMDb_trailer_in_new_tab.js
+Last updated: 2024.03.29
+
+Minifiy using https://www.minifier.org/
+javascript:(function(){var elem=document.querySelector("video.jw-video.jw-reset");var src=elem.getAttribute("src");window.open(src)})()
+*/
+
 javascript: (function () {
-    var elem = document.querySelector("video.jw-video.jw-reset");
-    var src = elem.getAttribute("src");
-    window.open(src);
+    var elem = document.querySelector("video.jw-video.jw-reset"); // identify element containing the video link
+    var src = elem.getAttribute("src");                           // extract video link
+    window.open(src);                                             // open link in new window/tab
 })();
